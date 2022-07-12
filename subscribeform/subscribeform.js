1bf8b4120e83dfbb4177f5dd5648ed49767cc4d4
vanessaReact <keliumju@gmail.com>
2022-07-12 12:14:12 -0500

    subscribe jquery ajax consume api+contact us confirm

--- subscribeform/subscribeform.js
+++ subscribeform/subscribeform.js
@@ -3,19 +3,19 @@ $(document).ready(function () {
   	$('form.formSubscribe').submit(function() {
 	var inputString = $("#email-subscribe").val();
 	//alert(inputString);
-	console.log(inputString);
-	$("#miModal").modal("show");
+	//console.log(inputString);
+	//$("#miModal").modal("show");
 
 	$.ajax({
-		url: "http://localhost:55987/api/PostEmpleado",
+		url: "http://127.0.0.1:5000/api/mail_user/create",
 		type: "Post",
 		data: JSON.stringify({
-			email: inputString,
+			mail: inputString,
 		}),
 		contentType: 'application/json; charset=utf-8',
 		success: function (data) {
 		$("#miModal").modal("show");
-		alert('Registro aregado exitosamente !!!');
+		//alert('Registro aregado exitosamente !!!');
 		},
 		failure: function (data)
 		{
